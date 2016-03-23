d18fef32a3dd4822708b429373e2de3e4708bf03
Micah Schow <micah.schow@gmail.com>
2016-03-23 16:01:24 -0600

    Adds todo API and API routing.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 import express from 'express';
 import config from 'config';
+import bodyParser from 'body-parser';
 import api from './routes'; //Root API route
 
 const app = express();
 const PORT = config.port;
 
 //Middleware
+app.use(bodyParser.json());
 app.use('/api', api);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- routes/todos.js
+++ routes/todos.js
@@ -1,14 +1,53 @@
 'use strict';
 
 import express from 'express';
+import Todo from '../lib/todo';
 
 const { Router } = express;
 const todosRouter = new Router();
 
-const todos = [];
+//const todos = [];
 
+//GETs all todos
 todosRouter.get('/', (req, res, next) => {
-  res.json(todos);
+  Todo.find((err, todos) => {
+    if (err) return next(err);
+    res.json(todos);
+  });
+});
+
+//POSTs a new todo
+todosRouter.post('/', (req, res, next) => {
+  Todo.create(req.body, (err, todo) => {
+    if (err) return next(err);
+    //Status code 201 means 'created' according to REST guidelines
+    res.status(201).json(todo);
+  });
+});
+
+//Retrieve a single todo by ID
+todosRouter.get('/:id', (req, res, next) =>{
+  Todo.findOne(req.params.id, (err, todo) => {
+    if (err) return next(err);
+    res.json(todo);
+  });
+});
+
+//Update a todo with PUT
+todosRouter.put('/:id', (req, res, next) => {
+  Todo.update(req.params.id, req.body, (err, todo) => {
+    if (err) return next(err);
+    res.json(todo);
+  });
+});
+
+//Delete a todo
+todosRouter.delete('/:id', (req, res, next) => {
+  Todo.delete(req.params.id, (err) => {
+    if (err) return next(err);
+    //A 204 status means 'no content'
+    res.sendStatus(204);
+  });
 });
 
 export default todosRouter;
